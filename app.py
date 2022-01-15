329b058e4096f5837b7e45863f80086673e9f3ed
Gautam Menghani <gum3ng@protonmail.com>
2022-01-15 22:12:04 +0530

    In-memory caching of urls using python dicts

--- app.py
+++ app.py
@@ -4,17 +4,26 @@ from helperFunctions import isURLValid, hashURL
 
 app = Flask(__name__)
 
+urls = dict()
+
 @app.route('/')
 def hello_world():
     return render_template('form.html')
 
 @app.route('/shortenURL', methods=['POST'])
 def shortenURL():
+    global urls
     url = request.form['url']
     if(not isURLValid(url)):
         return 'URL is invalid'
     else:
         #strip trailing slashes from URL
         url = url.rstrip('/')
-        hashedURL = "https://short.en/" + hashURL(url)
-        return hashedURL
+
+        #check if url is already hashed
+        if url in urls:
+            return urls[url]
+        else:
+            hashedURL = "https://short.en/" + hashURL(url)
+            urls[url] = hashedURL
+            return hashedURL
